beeaffb20e90b9d01bdd530b65ed1872d4d7c6e4
Валерия Яровцева <valeriayarovtseva@yandex.ru>
2022-07-14 15:05:40 +0300

    Вариант без подключения к БД

--- src/main/java/ru/alishev/springcourse/controller/PeopleController.java
+++ src/main/java/ru/alishev/springcourse/controller/PeopleController.java
@@ -1,2 +1,62 @@
-package ru.alishev.springcourse.controller;public class PeopleController {
+package ru.alishev.springcourse.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import ru.alishev.springcourse.dao.PersonDao;
+import ru.alishev.springcourse.model.Person;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+
+@Controller
+@RequestMapping("/people")
+public class PeopleController {
+
+    private final PersonDao personDao;
+
+    public PeopleController(PersonDao personDao) {
+        this.personDao = personDao;
+    }
+
+    @GetMapping
+    public String index(Model model) {
+        model.addAttribute("people", personDao.index());
+        return "people/index";
+    }
+
+    @GetMapping("{id}")
+    public String show(@PathVariable("id") int id, Model model) {
+        model.addAttribute("person", personDao.show(id));
+        return "people/show";
+    }
+
+    @GetMapping("/new")
+    public String newPerson(@ModelAttribute("person") Person person) {
+        return "people/new";
+    }
+
+    @PostMapping
+    public String create(@ModelAttribute("person") Person person) {
+        personDao.save(person);
+        return "redirect:/people";
+    }
+
+    @GetMapping("/{id}/edit")
+    public String edit(Model model, @PathVariable("id") int id) {
+        model.addAttribute("person", personDao.show(id));
+        return "people/edit";
+    }
+
+    @PatchMapping("/{id}")
+    public String update(@ModelAttribute("person") Person person, @PathVariable("id") int id) {
+        personDao.update(id, person);
+        return "redirect:/people";
+    }
+
+    @DeleteMapping("/{id}")
+    public String delete(@PathVariable("id") int id) {
+        personDao.delete(id);
+        return "redirect:/people";
+    }
 }
--- src/main/java/ru/alishev/springcourse/dao/PersonDao.java
+++ src/main/java/ru/alishev/springcourse/dao/PersonDao.java
@@ -1,2 +1,46 @@
-package ru.alishev.springcourse.dao;public class PersonDao {
+package ru.alishev.springcourse.dao;
+
+import org.springframework.stereotype.Component;
+import ru.alishev.springcourse.model.Person;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class PersonDao {
+    private static int PEOPLE_COUNT;
+    private List<Person> people;
+
+    {
+        people = new ArrayList<>();
+        people.add(new Person(++PEOPLE_COUNT, "Valeria"));
+        people.add(new Person(++PEOPLE_COUNT, "Alexander"));
+        people.add(new Person(++PEOPLE_COUNT, "Tatiana"));
+        people.add(new Person(++PEOPLE_COUNT, "Neil"));
+    }
+
+    public List<Person> index() {
+        return people;
+    }
+
+    public Person show(int id) {
+        return people.stream().filter(person -> person.getId()==id).findAny().orElse(null);
+    }
+
+    public void save(Person person) {
+        person.setId(++PEOPLE_COUNT);
+        people.add(person);
+    }
+
+    public void update(int id, Person updatedPerson) {
+        Person personToBeUpdated = show(id);
+        personToBeUpdated.setName(updatedPerson.getName());
+    }
+
+    public void delete(int id) {
+        people.removeIf(p -> p.getId()==id);
+    }
+
+
 }
+
